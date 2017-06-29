4214881ccdda98abf3c59ea302c2e631ac923e56
lurchpop <steve.haldane@gmail.com>
2017-06-29 02:14:03 -0700

    clean up api so is re-usable

--- generator-lite.js
+++ generator-lite.js
@@ -1,53 +1,70 @@
-var myPage = document.getElementById('mypage');
-var iframe = document.createElement('iframe');
+var GENERATOR_BASE_PATH = document.currentScript.baseURI;
 
-var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
-var eventer = window[eventMethod];
-var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+/**
+ * instance of generator in iframe 
+ * config - 
+ * config.components: STRING containing json to be passed to generator 
+ * targetDiv - inject iframe into this element
+ * @returns 
+ *          .basePath - baseURI being used 
+ *          .iframe - iframe element 
+ *          .init(config, targetDiv) -- this is run by default. don't call unless you want to re-init with new data
+ *          .getData() - promise returning {html, data}
+ * 
+*/
+function generator(config, targetDiv){
+  var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+  var eventer = window[eventMethod];
+  var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+  var iframe = document.createElement("iframe");
+  iframe.width=500; 
+  iframe.height=500;
+  var inited = false;
+  var ret = {
+    basePath: GENERATOR_BASE_PATH,
+    iframe: iframe, 
 
-var generator = {}
-
-generator.init = function(config) {
-  document.body.appendChild(iframe);
-
-  // iframe.src = 'http://gen.wevr.com/';
-  iframe.src = 'generator.html'
-  iframe.width = '800';
-  iframe.height = '800';
-
-  if (config) {
-    // send config to generator
-    return setTimeout(() => {iframe.contentWindow.postMessage({ call: 'configPage', config: config }, '*')}, 1000);
-  }
-
-  // Listen to messages from child window
-  eventer(messageEvent,function(e) {
-    if (e.data.html) {
-      // here is our html and data
-      return e.data;
-    }
-  },false);
-}
-
-generator.getData = function(config) {
-  var instId = "gen-get-data" + Math.random();
-  var messageConfig = config ? config : {};
-  messageConfig.id = instId;
-  messageConfig.call = 'getData';
-
-  iframe.contentWindow.postMessage(messageConfig, '*')
-
-  return new Promise(function(resolve, reject) {
-
-    var timer = setTimeout(function() {
-        return reject(new Error("Post message timed out"));
-    }, 2000);
-
-    eventer(messageEvent,function(e) {
-      if (e.data.id && e.data.id == instId) {
-        console.log('resolving', e.data.id, instId)
-        return resolve({ html: e.data.html, data: e.data.data });
+    init: function(config, targetDiv){
+      targetDiv.appendChild(iframe);
+      var load = function(){ 
+        inited=true;
+        iframe.contentWindow.postMessage({call: 'configPage', config: config}, "*");  
       }
-    },false);
-  });
-}
+      if(inited){
+        load();
+      } else {
+        iframe.contentWindow.addEventListener("load", load, {once: true});
+      }
+      iframe.src=GENERATOR_BASE_PATH+"generator.html";
+    },
+
+    getData: function(){
+      if(!inited){
+        return Promise.reject(new Error("not inited yet"));
+      }
+      var instId = "gen-get-data" + Math.random();
+      var config={
+        call: "getData",
+        id: instId, 
+        basePath: GENERATOR_BASE_PATH
+      }
+      console.log("getDataConfig", config);
+      iframe.contentWindow.postMessage(config, '*')
+      return new Promise(function(resolve, reject) {
+        var timer = setTimeout(function() {
+            return reject(new Error("Post message timed out"));
+        }, 2000);
+
+        eventer(messageEvent,function(e) {
+          if (e.data.id && e.data.id == instId) {
+            return resolve({ html: e.data.html, data: e.data.data });
+          }
+        },{once: true});
+      });
+    }//getData
+
+  }//init  
+  ret.init(config, targetDiv);
+  return ret;
+}//generator
+
