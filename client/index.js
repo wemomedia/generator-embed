f2953d6a590b01982bcaedf71cab01231a6afc14
Mike McCready <michaeljmccready@gmail.com>
2017-11-08 18:32:33 -0800

    move window listener to react index

--- client/index.js
+++ client/index.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 import { Router, browserHistory } from 'react-router';
 
-import { LOAD_COMPONENTS, SET_PAGE_DATA, GET_PAGE_DATA } from './actions/types';
+import { LOAD_COMPONENTS, SET_PAGE_DATA, GET_PAGE_DATA, CHANGE_EDITING_INDEX, TOGGLE_CONTROLLER_MENU, ADD_PLUGIN_COMPONENT } from './actions/types';
 import routes from './routes';
 import reducers from './reducers';
 
@@ -13,6 +13,9 @@ import reducers from './reducers';
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 const store = createStoreWithMiddleware(reducers);
 
+////////////////////////
+//embed init listeners//
+////////////////////////
 window.addEventListener('message', e => {
   if (e.data.call === 'configPage') {
     const componentData = JSON.parse(e.data.config.components);
@@ -31,6 +34,75 @@ window.addEventListener('message', e => {
     store.dispatch({ type: GET_PAGE_DATA, payload: e.data });
   }
 }, false);
+////////////////////////////
+//end embed init listeners//
+////////////////////////////
+
+
+/////////////////////////
+//start plugin listener//
+/////////////////////////
+window.addEventListener('generator-register-plugin', e => {
+  const fileUrls = e.detail.urls
+  var viewPromise = readComponentData(fileUrls.view)
+  var editorPromise = readComponentData(fileUrls.editor)
+  var dataPromise = readComponentData(fileUrls.data)
+
+  function readComponentData(url) {
+    return new Promise((resolve, reject) => {
+      //get component data from file
+      var client = new XMLHttpRequest();
+      client.open('GET', url);
+      client.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          const componentString = client.responseText;
+          resolve(componentString)
+        }
+      }
+      client.send();
+    })
+  }
+
+  Promise.all([viewPromise, editorPromise, dataPromise])
+  .then(values => {
+    const componentStrings = { view: values[0], editor: values[1], data: values[2] }
+    insertComponent(componentStrings)
+  });
+
+  function insertComponent(strings) {
+    const data = JSON.parse(strings.data);
+    // translate component from string to function
+    const viewTranslate = Babel.transform(strings.view, { presets: ['es2017', 'react'] }).code;
+    const editorTranslate = Babel.transform(strings.editor, { presets: ['es2017', 'react'] }).code;
+    const ViewComponent = eval('(' + viewTranslate + ')')
+    const EditorComponent = eval('(' + editorTranslate + ')')
+
+    // add component data to component index
+    const newComponent = { view: {}, editor: {} }
+    newComponent.view[data.title] = ViewComponent
+    newComponent.editor[data.title + 'Editor'] = EditorComponent
+
+    // props.addPluginComponent(newComponent, data)
+    store.dispatch({ type: ADD_PLUGIN_COMPONENT, payload: newComponent })
+    // add component to active component list
+    setTimeout(() => addComponent(null, data), 1500);
+  }
+
+  function addComponent(e, componentData) {
+    const newComponentsArray = store.getState().activeComponents.slice();
+    newComponentsArray.push(componentData);
+    store.dispatch({ type: LOAD_COMPONENTS, payload: newComponentsArray });
+    store.dispatch({ type: CHANGE_EDITING_INDEX, payload: newComponentsArray.length - 1 });
+    store.dispatch({ type: TOGGLE_CONTROLLER_MENU, payload: null });
+    setTimeout(() => {
+      window.scrollTo(0, document.body.scrollHeight)
+    }, 250);
+  }
+})
+////////////////////////
+// end plugin listener//
+////////////////////////
+
 
 const app = document.getElementById('app');
 
